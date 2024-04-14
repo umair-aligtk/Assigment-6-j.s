d1a6f9b5019fabec6f346d912432d3661deb887f
umair-aligtk <156018837+umair-aligtk@users.noreply.github.com>
2024-04-14 22:34:34 +0500

    main

---
+++ app.js
@@ -0,0 +1,50 @@
+
+
+// Q NO:1. Write a program that takes two user inputs for first and
+// last name using prompt and merge them in a new variable
+// titled fullName. Greet the user using his full name.
+
+// var firstName = prompt('Enter your first name')
+// var lastName = prompt('Enter your last name')
+// var fullName= firstName + lastName
+// if (fullName== firstName + lastName){
+// document.write(` ${fullName} Welcome to our  website`)
+// }
+
+// Q : no:2. Write a program to take a user input about his favorite
+// mobile phone model. Find and display the length of user
+// input in your browser
+
+// var user = prompt('Enter your favourite phone')
+// var userLength = user.length
+// document.write (`My favourite phone is: ${user} <br> length of string is: ${userLength}`)
+
+
+// Q No:3. Write a program to find the index of letter “n” in the word
+// “Pakistani” and display the result in your browser .
+
+// var one =  'pakistani'
+// var two = one.indexOf('n')
+// document.write(`string : ${one} <br> IndexOf 'n'is :${two}`)
+
+// Q NO:4. Write a program to find the last index of letter “l” in the
+// word “Hello World” and display the result in your browser.
+
+
+// var three =  'Hello world'
+// var four = three.lastIndexOf('l')
+// document.write(`string : ${three} <br>LastIndexOf 'l'is :${four}`)
+
+// Q No:5. Write a program to find the character at 3rd index in the
+// word “Pakistani” and display the result in your browser.
+// var five = 'Pakistani'
+// var six = five.charAt(3)
+// document.write (six)
+
+// Q no: 7. Write a program to replace the “Hyder” to “Islam” in the
+// word “Hyderabad” and display the result in your browser.
+
+// var city = 'Hyderabad'
+// var replace = city.replaceAll('Hyder','Islam')
+// document.write(replace)
+
